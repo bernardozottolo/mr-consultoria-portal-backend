80d996ad1d68848c2611cb75533b063201c9338e
Bernardo Zottolo <bernardo.zottolo@hotmail.com>
2026-01-11 21:27:18 -0300

    edit __init__.py

--- data/__init__.py
+++ data/__init__.py
@@ -1,1 +1,8 @@
 # Data API package
+# Exportar módulos para importação: from data import users_db, database, reports_db
+
+from . import users_db
+from . import database
+from . import reports_db
+
+__all__ = ['users_db', 'database', 'reports_db']
